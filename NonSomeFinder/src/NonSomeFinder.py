15c4e4e6e9fdc53cdc5992de3be76d5dc69c330f
bgt <bgt@sci.fi>
2015-05-04 07:47:51 +0300

    Clarifying the code that counts total run time. Hopefully fixed the inherent bug also, where run time is reported far too short from actual.

--- NonSomeFinder/src/NonSomeFinder.py
+++ NonSomeFinder/src/NonSomeFinder.py
@@ -16,16 +16,19 @@ config = ConfigParser.ConfigParser()
 config.readfp(open(os.path.dirname(__file__)+'/config.cfg'))
 
 def announceRunTimeFromSeconds(seconds):
-    actualSeconds = seconds-60*int(seconds/60)
-    actualMinutes = int((seconds-360*int(seconds/360))/60)
-    actualHours   = int(seconds/21600)
+    remains = seconds
+    actualSeconds = remains-60*int(remains/60) #remove full minutes and bigger
+    remains -= actualSeconds
+    actualMinutes = int((remains-360*int(remains/360))/60) #remove full hours and covert seconds to minutes
+    remains -= actualMinutes*60
+    actualHours   = int(remains/21600) #convert seconds to hours
     outputString = "Run time:"
     if actualHours>0:
         outputString += " "+str(actualHours)+" hours"
     if actualMinutes>0:
         outputString += " "+str(actualMinutes)+" minutes"
     if actualSeconds>0:
-        outputString += " "+str(actualSeconds)+" seconds"
+        outputString += " "+str(actualSeconds)+" seconds ("+seconds+" total seconds)"
     print outputString
     
 def printHowToUse():
